7c2f6c7fd76561b316fd3ab7d5ece682c8eb25a1
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-05-24 10:43:44 +0800

    fist commit

---
+++ src/main/java/com/jcorpse/proxypool/ProxyPoolApplication.java
@@ -0,0 +1,13 @@
+package com.jcorpse.proxypool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyPoolApplication.class, args);
+    }
+
+}
